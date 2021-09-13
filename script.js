c94294f569edb5915be62eac43a7df0d0afd8dc8
Brian Helms <bhelms2132@gmail.com>
2021-09-13 12:10:44 -0400

    Node list, event listner, call function and function started

---
+++ script.js
@@ -0,0 +1,14 @@
+//--Adds to a node list
+const boxes = document.querySelectorAll('.box')
+
+//--Event Listener
+window.addEventListener('scroll', checkBoxes)
+
+//--Runs Function
+checkBoxes()
+
+//Function
+function checkBoxes() {
+    //--Variable
+    console.log(window.innerHeigth)
+}
